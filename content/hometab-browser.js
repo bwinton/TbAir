4bd551dca0ab8bf400164a7243332037697fde9c
Blake Winton <bwinton@latte.ca>
2010-06-09 16:19:37 -0400

    Get a much prettier format for the messages.
    --HG--
    branch : jquery-tmpl

--- content/hometab-browser.js
+++ content/hometab-browser.js
@@ -94,29 +94,6 @@ function addContent(data) {
   entry.bind("click", function (e) {showMessages($(this))});
 }
 
-function getMessageBody(aMessageHeader) {
-  try {
-  let messenger = Components.classes["@mozilla.org/messenger;1"]
-                            .createInstance(Components.interfaces.nsIMessenger);
-  let listener = Components.classes["@mozilla.org/network/sync-stream-listener;1"]
-                           .createInstance(Components.interfaces.nsISyncStreamListener);
-  let uri = aMessageHeader.folder.getUriForMsg(aMessageHeader);
-  messenger.messageServiceFromURI(uri)
-           .streamMessage(uri, listener, null, null, false, "");
-  let folder = aMessageHeader.folder;
-  return folder.getMsgTextFromStream(listener.inputStream,
-                                     aMessageHeader.Charset,
-                                     65536,
-                                     32768,
-                                     false,
-                                     true,
-                                     { });
-  } catch (e) {
-    dump("Caught error in getMessageBody.  e="+e+"\n");
-    return "";
-  }
-}
-
 function addMessage(message) {
   let conversations = $("ol.conversations");
   if (conversations.length == 0)
@@ -131,14 +108,20 @@ function addMessage(message) {
   $('<span class="from"/>').text(message.from.value).appendTo(msg);
   $('<span class="date"/>').text(""+message.date).appendTo(msg);
 
-  let body = getMessageBody(message.folderMessage);
-  $('<div class="fullbody"/>').appendTo(msg).text(body).css("display", "none");
-  let synopsis = body;
-  if (message.indexedBodyText)
-    synopsis = message.indexedBodyText;
-  $('<div class="synopsis">').appendTo(msg).text(synopsis.substr(0, 140));
+  let body = $('<div class="fullbody"/>').appendTo(msg).css("display", "none");
+  let synopsis = $('<div class="synopsis">').appendTo(msg);
+  if (message.indexedBodyText) {
+    synopsis.text(message.indexedBodyText.substr(0, 140));
+    synopsis = null;
+  }
+//  populateMessageBody(message.folderMessage, body, synopsis);
 
   msg.bind("click", function (e) {showMessage($(this))});
+}
+
+function populateMessageBody(id, data) {
+  let body = $("#"+id).find(".fullbody");
+  body.html(data.documentElement.children);
 }
 
 const Cc = Components.classes;
